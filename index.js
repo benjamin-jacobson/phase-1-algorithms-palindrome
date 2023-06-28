0f39d440d88ac0da1e5ef8eea756d83031541a9d
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-06-28 10:50:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordReverse = word.split("").reverse().join("");
+
+  if (word === wordReverse){
+    return true 
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
+// get reverse of word. check if equal. if equal return true, else not
 /*
   Add written explanation of your solution here
 */
+// its a function with conditional logic returning true or false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
